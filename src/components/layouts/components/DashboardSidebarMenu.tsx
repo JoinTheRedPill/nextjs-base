20361e2ab603ea47fb0ca4c3716661868bdabac9
Isaac Acosta López <isaac.acosta.lop@gmail.com>
2022-07-11 17:01:49 +0100

    sidebar menu

--- src/components/layouts/components/DashboardSidebarMenu.tsx
+++ src/components/layouts/components/DashboardSidebarMenu.tsx
@@ -3,10 +3,10 @@ const DashboardSidebarMenu = () => {
     <div className="px-4 py-4 h-full">
       <div className="flex flex-col h-full">
         <div className="grow overflow-hidden overflow-y-auto">
-          <p>Menú lateral</p>
+          <p>Sidebar menu</p>
         </div>
 
-        <div className="flex-none">Footer menú lateral</div>
+        <div className="flex-none">Sidebar menu footer</div>
       </div>
     </div>
   );
